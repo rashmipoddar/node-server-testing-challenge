d2768fb2df31756d1813a0b680e9f8435fd4d44c
Rashmi <poddar.rashmi@gmail.com>
2019-11-15 10:58:54 -0500

    Added some more tests

--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 
 const server = require('./server');
+const db = require('./data/dbConfig');
 
 describe('it should check the environment', () => {
   it('should set db environment to testing', function() {
@@ -8,7 +9,7 @@ describe('it should check the environment', () => {
   })
 })
 
-describe('server', () => {
+describe('server endpoints testing', () => {
   describe('GET /', () => {
     it('should return status code 200', () => {
       return request(server)
@@ -43,14 +44,40 @@ describe('server', () => {
       .then(res => {
         // console.log(res);
         expect(res.status).toBe(200);
-        expect(res.type).toMatch(/json/i);
         expect(res.body.length).toEqual(2);
       })
       // .catch(err => {
       //   expect(res.status).toBe(200);
-      // })
+      // })  
     });
 
+    it('should return json formatted data', async() => {
+      const res = await request(server).get('/blogs');
+      expect(res.type).toMatch(/json/i);
+    });
+
+    it('should return array of blogs', async () => {
+      const res = await request(server).get('/blogs');
+      expect(res.body.length).toBeDefined();
+      expect(res.body[0]).toHaveProperty('title');
+    });
+
+  });
+
+  describe('add a blog', () => {
+    describe('POST /blogs', () => {
+      const blog = {title: 'Blog 1'};
+        beforeEach(async () => {
+            await db('blogs').truncate();
+        });
+
+        it('should return status 201', async () => {
+          const res = await request(server)
+              .post('/blogs')
+              .send(blog);
+          expect(res.status).toBe(201);
+      });
+    })
   });
 
 });
